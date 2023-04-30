7ba0b86141ecada8237154388cf22b11d3e4ad3a
tahmidachoudhury <tahmidachoudhury@outlook.com>
2023-04-30 10:44:20 +0100

    fix backend function + UI control-flow

--- Zakat.calc2.py
+++ Zakat.calc2.py
@@ -1,13 +1,13 @@
 def silverthreshold():
     #price of silver - changes in stocks
     silver = input("Please type the current price of silver per gram - remember to use GBP:\n")
-    try:
-        #threshold calc according to Musnad Ahmad 1233
-        nisaab = 595*float(silver) 
-        return ("The nisaab (threshold) of Zakat at this price of silver is £ ", nisaab)
-    except:
-        print("Please type a number.\n")
-        silverthreshold()
+    
+    #threshold calc according to Musnad Ahmad 1233
+    nisaab = 595*float(silver) 
+    return ("The nisaab (threshold) of Zakat at this price of silver is £ ", nisaab)
+    #except:
+     #   print("Please type a number.\n")
+      #  silverthreshold()
 
 def goldthreshold():
     gold = input("Please type the current price of gold per gram - remember to use GBP:\n")
@@ -40,6 +40,14 @@ print("")
 #make this alot user friendly and neat
 option1 = input("Feel free to use this calculator at any time during the year. However before use, would you like a short reading on how this program works?: (y/n) \n")
 greeting(option1)
+goldorsil = input("Would you like to calculate with the gold or silver threshold (we recommend silver)\n")
+if goldorsil == 'gold':
+    goldthreshold()
+if goldorsil == 'silver':
+    silverthreshold()
+else:
+    print('fin')
+    
 
 
 
